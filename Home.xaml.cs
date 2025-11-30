5b7fda9e09967381663f1cb0b5c26c77328bca99
iamnotlucid <143038382+iamnotlucid@users.noreply.github.com>
2025-11-30 14:58:10 +0000

    Add project files.

---
+++ Home.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Maron
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+
+            List<StudySet> studySets = new List<StudySet>
+            {
+                new StudySet("Biology Basics", "Biology", DateTime.Now.AddDays(3)),
+                new StudySet("World War II", "History", DateTime.Now.AddDays(5))
+            };
+
+            StudySets.ItemsSource = studySets;
+        }
+    }
+
+    public class StudySet 
+    {
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public DateTime DueDate { get; set; }
+        public static int TotalSets { get; set; } = 0;
+        public static int SetsCompletedToday { get; set; } = 0;
+
+        public StudySet(string name, string subject, DateTime duedate)
+        {
+            Name = name;
+            Subject = subject;
+            DueDate = duedate;
+
+            TotalSets++;
+        }
+
+        
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace Maron
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void HomePage(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Home());
+        }
+        public void SetsPage(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Sets());
+        }
+        public void StatsPage(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Stats());
+        }
+        public void SettingsPage(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Settings());
+        }
+    }
+}
